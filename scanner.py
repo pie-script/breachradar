e593bc74adbeab15d31ae0969b938f9a52b2cd1c
Idrish-Vasan <vasanidrish@gmail.com>
2026-09-17 15:06:47 +0530

    feat: add GoogleSearch import and define SerpApi query parameters

--- scanner.py
+++ scanner.py
@@ -1,6 +1,11 @@
 import os
 from dotenv import load_dotenv
+from serpapi import GoogleSearch
 
 load_dotenv()
 
 SERPAPI_API_KEY = os.getenv("SERPAPI_API_KEY")
+params ={
+    "q":"cofee",
+    "api_key": SERPAPI_API_KEY
+}
